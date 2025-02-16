5cac23ca186c801f0d63af133a5dace164b7318a
윤지현 <139779783+jheony@users.noreply.github.com>
2025-02-16 23:45:28 +0900

    [Bronze V] Title: 오늘 날짜, Time: 100 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+        public static void main(String[] args) {
+        System.out.println("2025-02-16");
+        }
+    }

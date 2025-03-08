54d3e5965b37cd7c1dbbacee83b38be72b9b476f
윤지현 <139779783+jheony@users.noreply.github.com>
2025-03-08 13:45:56 +0900

    [Gold V] Title: 회의실 배정, Time: 480 ms, Memory: 44280 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+       
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2){
+                if(o1[1] == o2[1])
+                    return o1[0] - o2[0];
+                return o1[1] - o2[1];
+            }
+        });
+        int end = 0, cnt = 0;
+        for (int[] i : arr) {
+            if(i[0] >= end){
+                end = i[1];
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}

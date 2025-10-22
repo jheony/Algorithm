17ee189e7a5fbc0d0fa921984e6316eb352bc2fe
윤지현 <139779783+jheony@users.noreply.github.com>
2025-10-22 09:18:38 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 47.35 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,11 +1,8 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
-        for(int i=1; i<=n; i++){
-            if(i%2!=0)
-                answer+="수";
-            else
-                answer+="박";
+        for(int i=0; i<n; i++){
+            answer += (i%2==0) ? "수" : "박" ;
         }
         return answer;
     }

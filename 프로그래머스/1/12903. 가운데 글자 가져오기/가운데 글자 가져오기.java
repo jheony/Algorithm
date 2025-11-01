34aa1fef476040fb98bb90606ddcf3261400153d
윤지현 <139779783+jheony@users.noreply.github.com>
2025-11-01 21:17:20 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 15.71 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        int len = s.length();
+        String answer = "";
+
+        if (len % 2 == 0) {
+            answer = s.charAt(len / 2 - 1) + "";
+        }
+        answer += s.charAt(len / 2) + "";
+
+        return answer;
+    }
+}

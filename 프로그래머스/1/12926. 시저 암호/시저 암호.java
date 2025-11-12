4b2cc3bf079cae12b933f2b71b341548a5b58444
윤지현 <139779783+jheony@users.noreply.github.com>
2025-11-12 09:28:32 +0900

    [level 1] Title: 시저 암호, Time: 18.60 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -3,9 +3,12 @@ class Solution {
         String answer = "";
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
+            // 소문자 or 대문자 구분
+            // 마지막 글자 넘었을 때 a로 돌아오기
             if((c+n > 'z' && c >= 'a') || (c+n > 'Z' && c <= 'Z')){
                 c -= 26;
             }
+            // 공백은 그대로 or n만큼 밀기
             answer += (c == ' ')?" ":(char)(c+n);
         }
         return answer;

c07b4cc19219a657efff3e6ed53b1f83b91ad117
윤지현 <139779783+jheony@users.noreply.github.com>
2025-11-13 09:27:37 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 7.92 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -5,7 +5,9 @@ class Solution {
         String[] eng = {"zero", "one", "two", "three", "four",
                         "five", "six", "seven", "eight", "nine"};
         for(int i=0; i<=9; i++){
+            // 입력받은 문자열에 영단어가 포함되어 있다면 
             if(s.contains(eng[i])){ 
+                // 숫자로 변환
                 s = s.replaceAll(eng[i], i+"");
             }
         }

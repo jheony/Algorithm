db9217a1b4571cefbe2d2cb834384203d7666b64
윤지현 <139779783+jheony@users.noreply.github.com>
2025-11-28 23:53:19 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,78 +2,55 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class point{
-    int x;
-    int y;
-    point(int nx, int ny){
-        this.x = nx;
-        this.y = ny;
-    }
-}
 class Main {
-    static int n;
-    static int[][] arr;
-    static boolean[][] check;
-    static ArrayList<Integer> cnt;
-    static Queue<point> q;
+    static int n, cnt=0, totalCnt=0;
+    static boolean visited[][];
+    static char arr[][];
     
-    public static void bfs(int nx, int ny){
-        q = new LinkedList<>();
-        q.add(new point(nx, ny));
-        check[nx][ny] = true;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new char[n][n];
+        visited = new boolean[n][n];
+        for (int i=0; i<n; i++){
+            arr[i] = br.readLine().toCharArray(); 
+        }
+        List<Integer> homeCnt = new ArrayList<>();
         
-        int[] Xarr = {0, 1, 0, -1};
-        int[] Yarr = {1, 0, -1, 0};
-        int xnext, ynext, c=0;
-        
-        while (!q.isEmpty()) {
-            point p = q.poll();
-            c++;
-            
-            for(int i=0; i<4; i++){
-                xnext = p.x + Xarr[i];
-                ynext = p.y + Yarr[i];
-
-                if(xnext >= 0 && ynext >= 0 && xnext < n && ynext < n 
-                   && !check[xnext][ynext] && arr[xnext][ynext] == 1){
-                    
-                    q.add(new point(xnext, ynext));
-                    check[xnext][ynext] = true;
+        for (int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j] == '1' && !visited[i][j]){
+                    cnt=0;
+                    dfs(i, j);
+                    totalCnt++;
+                    homeCnt.add(cnt);
                 }
             }
         }
-        cnt.add(c);
+        
+        System.out.println(totalCnt);
+        
+        Collections.sort(homeCnt);
+        for(int i:homeCnt){
+            System.out.println(i);
+        }
     }
-    
-    public static void main(String[] args)throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+    public static void dfs(int x, int y){
+        visited[x][y] = true;
+        cnt++;
         
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
-        check = new boolean[n][n];
-        cnt = new ArrayList<>();
-        String s;
-        
-        for(int i=0; i<n; i++){
-            s = br.readLine();
-            
-            for(int j=0; j<n; j++){
-                arr[i][j] = s.charAt(j)-'0';
-            }
-        }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(arr[i][j]==1 && !check[i][j])
-                    bfs(i, j);
-            }
-        }
+        int[] Xarr = {1, 0, -1, 0};
+        int[] Yarr = {0, -1, 0, 1};
 
-        Collections.sort(cnt);
-        sb.append(cnt.size() +"\n");
-        for(int i:cnt){
-            sb.append(i+"\n");
+        for(int i=0; i<4; i++){
+            int nextX = x + Xarr[i];
+            int nextY = y + Yarr[i];
+            
+            if(0<=nextX && nextX<n && 0<=nextY&&nextY<n){
+                if(arr[nextX][nextY] == '1' && !visited[nextX][nextY]){
+                    dfs(nextX, nextY);
+                }
+            }
         }
-        System.out.print(sb);
     }
 }

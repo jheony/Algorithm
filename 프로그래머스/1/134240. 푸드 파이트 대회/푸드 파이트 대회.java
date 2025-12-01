a8007e1e2630d042a11ec7ad67da120bceb82493
윤지현 <139779783+jheony@users.noreply.github.com>
2025-12-01 09:55:37 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.57 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,8 +2,11 @@ class Solution {
     public String solution(int[] food) {
         String answer = "";
         StringBuilder sb = new StringBuilder();
+        
+        // 1번 음식부터 끝까지
         for(int i=1; i<food.length; i++){
             int cnt = food[i];
+            // 두 선수가 먹는 음식의 양이 같아야 함
             for(int j=0; j<cnt/2; j++){
                 sb.append(i);
             }

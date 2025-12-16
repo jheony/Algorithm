25a0924fe2c47b347cf4033944c712ec57c475e6
윤지현 <139779783+jheony@users.noreply.github.com>
2025-12-16 09:05:30 +0900

    [level 1] Title: 과일 장수, Time: 50.69 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,13 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
+        
         Arrays.sort(score);
         
-        int i=score.length - m;
-        while(i>=0){
+        int i = score.length - m;
+        
+        while (i >= 0) {
             answer += score[i] * m;
             i -= m;
         }
+        
         return answer;
     }
 }

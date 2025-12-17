4ca7a632ac0f170f6619758fe24edfeda3c1f27e
윤지현 <139779783+jheony@users.noreply.github.com>
2025-12-17 11:01:48 +0900

    [level 1] Title: 소수 만들기, Time: 14.39 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -4,6 +4,7 @@ class Solution {
 
         int sum = 0;
         int len = nums.length;
+        
         for(int i=0; i<len-2; i++){
             for(int j=i+1; j<len-1; j++){
                 for(int k=j+1; k<len; k++){
@@ -13,10 +14,10 @@ class Solution {
                 }
             }
         }
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-
+        
         return answer;
     }
+    
     public static boolean prime(int x){
         for(int i=2; i<x; i++){
             if(x%i == 0) return false;

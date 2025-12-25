9a53282defe0b3b29e8f164d0cf579d3154f7884
윤지현 <139779783+jheony@users.noreply.github.com>
2025-12-25 23:30:37 +0900

    [Silver II] Title: 스택 수열, Time: 280 ms, Memory: 27920 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -3,29 +3,32 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
         
-        int n = Integer.parseInt(br.readLine()), x, r=2, popInt;
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        sb.append("+\n");
-       
+        int n = Integer.parseInt(br.readLine());
+        int inputNum, popInt = 0, number=1;
+
         for(int i=0; i<n; i++){
-            x = Integer.parseInt(br.readLine());
-            while (r <= x) {
-                s.push(r++);
+            inputNum = Integer.parseInt(br.readLine());
+            
+            while (number <= inputNum) {
+                stack.push(number++);
                 sb.append("+\n");
             }
-            popInt = s.pop();
+            
+            popInt = stack.pop();
             sb.append("-\n");
-            if(popInt>x){
-                sb = new StringBuilder();
-                sb.append("NO");
+
+            if(popInt > inputNum){
+                sb = new StringBuilder("NO");
                 break;
             }
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
     }
 }
